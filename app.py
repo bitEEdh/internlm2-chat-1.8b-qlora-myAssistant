6aeb1ee65bc5b42e37218178864976eff5f1df41
Hang Deng <79652724+bitEEdh@users.noreply.github.com>
2024-04-26 23:09:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import os
 base_path = './final_model'
 os.system(f'git clone https://code.openxlab.org.cn/bitEEdh/internlm2-chat-1.8b-qlora-myAssistant.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
-os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
+os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7861')
